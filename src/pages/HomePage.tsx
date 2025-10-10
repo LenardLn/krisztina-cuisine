fed56965a1b49a732d92cafe98613cde8ab2a69e
LenardLn <nagylenard2002@gmail.com>
2025-10-10 17:16:31 +0300

    updated with the chocolate mousse

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,51 @@
 import { Link } from "react-router-dom";
 import "../css/home.css";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [borderState, setBorderState] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBorderState((prev) => !prev);
+    }, 1200);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="intro">
       <header>
-        <h1> Bun venit la Krisztina Cuisine 👋</h1>
+        <h1>Bun venit la Krisztina Cuisine 👋</h1>
         <h2 style={{ margin: "1rem", color: "red" }}>
-          <span style={{ textDecoration: "underline" }}>NOU</span> Pască cu
-          brânză dulce! 36lei/kg
+          <span style={{ textDecoration: "underline" }}>NOU</span> Tort cu
+          mousse de ciocolată cu lapte și ciocolată albă 80lei/kg
         </h2>
-        <h2 style={{ margin: "1rem", color: "red" }}>
-          <span style={{ textDecoration: "underline" }}>NOU</span> Cosuleț cu
-          gem de caise și bezea cu nucă 50lei/kg
-        </h2>
+        <img
+          className="nou-praji"
+          src="/assets/mousse.svg"
+          alt="mousse picture"
+          style={{
+            borderColor: borderState ? "hotpink" : "pink",
+            boxShadow: `0 0 20px 7.5px  ${borderState ? "hotpink" : "pink"}`,
+          }}
+        />
       </header>
       <p style={{ fontWeight: "bold" }}>
         Pe această pagină puteți vedea catalogul meu de prăjituri!
       </p>
+
       <a
         href="tel:+40744827373"
-        style={{ textDecoration: "none", color: "black" }}
+        style={{
+          textDecoration: "none",
+          color: "black",
+          border: "3px solid",
+          borderRadius: "12px",
+          padding: "0.5rem 1rem",
+          display: "inline-block",
+          transition: "border-color 0.6s ease",
+        }}
       >
         <p className="telefon">
           Apăsați să sunați <br /> 0744827373
@@ -28,7 +53,7 @@ const HomePage = () => {
       </a>
 
       <h2 className="h2">
-        <Link to={"/prajituri"}>La prăjituri </Link>
+        <Link to={"/prajituri"}>La prăjituri</Link>
       </h2>
     </div>
   );
